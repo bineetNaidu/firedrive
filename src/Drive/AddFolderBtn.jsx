56eda32081a8886c12a965e989e3303e7ffe5207
bineetNaidu <bineet.s.naidu@gmail.com>
2021-02-12 23:25:25 +0530

    ✨ add Folder Button Component

---
+++ src/Drive/AddFolderBtn.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFolderPlus } from '@fortawesome/free-solid-svg-icons';
+import { database } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function AddFolderBtn({ currentFolder }) {
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState('');
+  const { currentUser } = useAuth();
+
+  const toggleModalState = () => {
+    setOpen((prevState) => !prevState);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (currentFolder == null) return;
+    if (!name) return;
+
+    // Create a Folder in storage bucket;
+    database.folders.add({
+      name,
+      parentId: currentFolder.id,
+      userId: currentUser.uid,
+      // path,
+      createdAt: database.getCurrentTimestamp(),
+    });
+    setName('');
+    toggleModalState();
+  };
+  return (
+    <>
+      <Button variant="outline-success" size="sm" onClick={toggleModalState}>
+        <FontAwesomeIcon icon={faFolderPlus} />
+      </Button>
+
+      <Modal show={open} onHide={toggleModalState}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type="text"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={toggleModalState}>
+              Close
+            </Button>
+            <Button variant="success" type="submit">
+              Add Folder
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
+  );
+}
--- src/Drive/Dashboard.jsx
+++ src/Drive/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import AddFolderBtn from './AddFolderBtn';
 import DriveNavbar from './Navbar';
 
 const Dashboard = () => {
   return (
     <>
       <DriveNavbar />
-      <Container fluid>Content</Container>
+      <Container fluid>
+        <AddFolderBtn />
+      </Container>
     </>
   );
 };
